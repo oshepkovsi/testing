fa2ef3cb178c9ec372ad5d1d7a239aa5804d7a56
oshepkovsi <114593045+oshepkovsi@users.noreply.github.com>
2022-10-05 19:59:55 +0400

    Add files via upload

---
+++ huina.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+
+
+echo 'Let`s get started'
+apt update && apt upgrade -y
+echo 'Apt updated'
+apt install mc make git openssl libpcre++-dev libssl-dev libxslt1-dev libgeoip-dev libgd-dev gcc -y
+echo 'We install some bullshit'
+mkdir /var/lib/nginx
+touch /var/lib/nginx/body
+#mkdir /git
+sh git.sh
+
+
+#git init
+#git remote add origin https://github.com/oshepkovsi/testing.git
+#git pull https://github.com/oshepkosi/testing.git
+cd /git/
+echo 'Now we download all needed files'
+
+tar xzf echo-nginx-module-0.62.tar.gz
+tar xzf nginx-1.23.1.tar.gz
+tar xzf nginx-module-vts-0.2.1.tar.gz
+tar xzf nginx-rtmp-module-master.tar.gz
+echo 'Unzipping needed files'
+#----------------------
+cd /git/nginx-1.23.1/
+
+./configure --with-cc-opt='-g -O2 -fPIE -fstack-protector-strong -Wformat -Werror=format-security -D_FORTIFY_SOURCE=2' --with-ld-opt='-Wl,-Bsymbolic-functions -fPIE -pie -Wl,-z,relro -Wl,-z,now' --prefix=/usr/share/nginx --conf-path=/etc/nginx/nginx.conf --http-log-path=/var/log/nginx/access.log --error-log-path=/var/log/nginx/error.log --lock-path=/var/lock/nginx.lock --pid-path=/run/nginx.pid --http-client-body-temp-path=/var/lib/nginx/body --http-fastcgi-temp-path=/var/lib/nginx/fastcgi --http-proxy-temp-path=/var/lib/nginx/proxy --http-scgi-temp-path=/var/lib/nginx/scgi --http-uwsgi-temp-path=/var/lib/nginx/uwsgi --with-debug --with-pcre-jit --with-http_ssl_module --with-http_stub_status_module --with-http_realip_module --with-http_auth_request_module --with-http_addition_module --with-http_dav_module --with-http_geoip_module --with-http_image_filter_module --with-http_v2_module --with-http_sub_module --with-http_xslt_module --with-stream --with-stream_ssl_module --with-mail --with-mail_ssl_module --without-http_gzip_module --with-threads --add-module=/git/echo-nginx-module-0.62
+
+make && make install
+
+cp /usr/share/nginx/sbin/nginx /usr/sbin/nginx
+cp /git/nginx.service /lib/systemd/system/nginx.service
+systemctl stop nginx
+systemctl enable nginx
+systemctl stop nginx
+
+#----------------------
+./configure --with-cc-opt='-g -O2 -fPIE -fstack-protector-strong -Wformat -Werror=format-security -D_FORTIFY_SOURCE=2' --with-ld-opt='-Wl,-Bsymbolic-functions -fPIE -pie -Wl,-z,relro -Wl,-z,now' --prefix=/usr/share/nginx --conf-path=/etc/nginx/nginx.conf --http-log-path=/var/log/nginx/access.log --error-log-path=/var/log/nginx/error.log --lock-path=/var/lock/nginx.lock --pid-path=/run/nginx.pid --http-client-body-temp-path=/var/lib/nginx/body --http-fastcgi-temp-path=/var/lib/nginx/fastcgi --http-proxy-temp-path=/var/lib/nginx/proxy --http-scgi-temp-path=/var/lib/nginx/scgi --http-uwsgi-temp-path=/var/lib/nginx/uwsgi --with-debug --with-pcre-jit --with-http_ssl_module --with-http_stub_status_module --with-http_realip_module --with-http_auth_request_module --with-http_addition_module --with-http_dav_module --with-http_geoip_module --with-http_image_filter_module --with-http_v2_module --with-http_sub_module --with-http_xslt_module --with-stream --with-stream_ssl_module --with-mail --with-mail_ssl_module --without-http_gzip_module --with-threads --add-module=/git/echo-nginx-module-0.62 --add-dynamic-module=/git/nginx-rtmp-module-master --add-dynamic-module=/git/nginx-module-vts-0.2.1 
+
+
+make modules
+
+
+
+
